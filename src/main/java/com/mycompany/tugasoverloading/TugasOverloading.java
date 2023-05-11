f808889ad7f1881bf2d7233b215c431af43630b9
naffsvn <naffsvn@protonmail.com>
2023-05-12 01:09:40 +0700

    v1.1

--- src/main/java/com/mycompany/tugasoverloading/TugasOverloading.java
+++ src/main/java/com/mycompany/tugasoverloading/TugasOverloading.java
@@ -9,48 +9,62 @@ package com.mycompany.tugasoverloading;
  * @author naffsisky
  */
 public class TugasOverloading {
+    private int[] x;
+    private int[] y;
+    private int skalar;
+    
+    public TugasOverloading(int[] x, int[] y, int skalar) {
+        this.x = x;
+        this.y = y;
+        this.skalar = skalar;
+    }
+
     public static void main(String[] args) {
         int[] x = {1, 2, 3};
         int[] y = {4, 5, 6};
         int skalar = 5;
 
-        int hasilVectorSkalar = vectorSkalar(x, skalar);
-        System.out.println("Hasil vektor X dikali dengan Skalar " + skalar  + " adalah : " + hasilVectorSkalar);
-        int hasilVectorVector = vectorVector(x, y);
+        TugasOverloading obj = new TugasOverloading(x, y, skalar);
+
+        int hasilVectorSkalar = obj.vectorSkalar();
+        System.out.println("Hasil vektor X dikali dengan Skalar " + skalar + " adalah : " + hasilVectorSkalar);
+
+        int hasilVectorVector = obj.vectorVector();
         System.out.println("Hasil vektor X dikali dengan vektor Y adalah : " + hasilVectorVector);
-        
     }
-    public static int vectorSkalar(int[] x, int skalar){
+
+    public int vectorSkalar() {
         int hasil = 0;
         int j = x.length - 1;
         int z = 0;
-        
-        for(int i = 0; i < x.length; i++){
+
+        for (int i = 0; i < x.length; i++) {
             hasil += x[i] * skalar;
             z += x[i] * skalar;
-            System.out.print(x[i] + skalar);
-            if(i != j){
+            System.out.print(x[i] + "*" + skalar);
+            if (i != j) {
                 System.out.print(" + ");
             }
-            if(i == j){
+            if (i == j) {
                 System.out.println(" = " + z);
             }
         }
         return hasil;
     }
-    public static int vectorVector(int[] x, int[] y){
+
+    public int vectorVector() {
         int hasil = 0;
         int j = x.length - 1;
         int z = 0;
-        
-        for(int i = 0; i < x.length; i++){
+
+        for (int i = 0; i < x.length; i++) {
             hasil += x[i] * y[i];
             z += x[i] * y[i];
-            System.out.print(x[i] + y[i]);
-            if(i != j){
+            System.out.print(x[i] + "*" + y[i]);
+            if (i != j) {
                 System.out.print(" + ");
             }
-            if(i == j){
+            if (i == j) {
                 System.out.println(" = " + z);
             }
         }
